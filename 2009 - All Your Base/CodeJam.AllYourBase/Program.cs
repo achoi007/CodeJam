026b9f47266b702856b2199a4ef14679a832957c
A Choi <andy.choi@live.com>
2015-02-04 22:11:11 -0600

    First attempt at solution for All Your Base.

---
+++ 2009 - All Your Base/CodeJam.AllYourBase/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeJam.AllYourBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read input lines
+            string[] inputs = File.ReadAllLines(args[0]);
+
+            // Generate solution
+            var solver = new Solver();
+            solver.Analyze(inputs);
+            var solution = solver.Solution;
+
+            // Print out solution
+            var solnTexts = solution.Select((n, i) =>
+            {
+                return string.Format("Case #{0}: {1}", i, n);
+            });
+            File.WriteAllLines(args[1], solnTexts);
+        }
+    }
+}
